fb6a6e072833372f6e11e81211df8beec99909d4
AdityaThyagaraju <111631888+AdityaThyagaraju@users.noreply.github.com>
2024-02-20 19:32:47 +0530

    finished set up

---
+++ myapp/src/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Signup(){
+    return (
+        <div>
+            Signup
+        </div>
+    )
+}
+
+export default Signup;
